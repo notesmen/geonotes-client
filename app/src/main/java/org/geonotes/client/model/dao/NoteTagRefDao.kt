83d97a3502c31b13b759c538572042e9990a148a
Alexey Filinovich <aliaks35@gmail.com>
2020-12-16 20:17:20 +0300

    New note storage functions

--- app/src/main/java/org/geonotes/client/model/dao/NoteTagRefDao.kt
+++ app/src/main/java/org/geonotes/client/model/dao/NoteTagRefDao.kt
@@ -1,11 +1,8 @@
 package org.geonotes.client.model.dao
 
 import androidx.paging.DataSource
-import androidx.room.Dao
-import androidx.room.Insert
+import androidx.room.*
 import androidx.room.OnConflictStrategy.*
-import androidx.room.Query
-import androidx.room.Transaction
 
 import org.geonotes.client.model.entity.Note
 import org.geonotes.client.model.entity.NoteBaseTagCrossRef
@@ -14,6 +11,9 @@ import org.geonotes.client.model.entity.NoteBaseTagCrossRef
 interface NoteTagRefDao {
     @Insert(onConflict = IGNORE)
     suspend fun save(noteBaseTagCrossRef: NoteBaseTagCrossRef)
+
+    @Query("DELETE FROM NoteBaseTagCrossRef WHERE noteId = :noteId")
+    suspend fun deleteNoteTagRefs(noteId: Long)
 
     @Transaction
     @Query("SELECT * FROM NoteBase ORDER BY lastChangeTime DESC")
